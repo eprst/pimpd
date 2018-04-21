14da7a418a013350ae2b29f6cd541dd7f77c04c7
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2018-04-21 02:15:52 -0700

    progress bar widget

--- pimpd/Test2.py
+++ pimpd/Test2.py
@@ -11,6 +11,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from stext import ScrollingText
+from pbar import ProgressBar
 
 # Rotate by 180? That's the way I have my PI positioned, bonnet joystick on the right
 ROTATE = True
@@ -59,13 +60,22 @@ image = Image.new('1', (width, height))
 
 font = ImageFont.truetype("DejaVuSans.ttf", 12)
 stext = ScrollingText((10,10), (80,20), font, u'Hello::Привет!')
+pbar = ProgressBar((10,40), (80,10), 100)
+val = 0
 
 try:
     while 1:
         draw = ImageDraw.Draw(image)
         draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+        if not GPIO.input(L_pin):
+            val = max(0, val-5)
+        if not GPIO.input(R_pin):
+            val = min(100, val+5)
+        pbar.set_value(val)
         
         stext.refresh(image, draw)
+        pbar.refresh(image, draw)
         
         if ROTATE:
             image = image.rotate(180)
---
+++ pimpd/pbar.py
@@ -0,0 +1,21 @@
+from PIL import ImageDraw, Image
+from widget import Widget
+
+class ProgressBar(Widget):
+    def __init__(self, position, size, max_value):
+        # type: (ProgressBar, (int, int), (int, int), float -> None
+        super(ProgressBar, self).__init__(position, size)
+        self._max_value = max_value
+        self._value = 0
+
+    def set_value(self, value):
+        # type: float -> None
+        assert value >= 0 and value <= self._max_value
+        self._value = value
+
+    def _draw(self, draw):
+        # type: (ProgressBar, ImageDraw) -> None
+        len = self._value * self._size[0] / self._max_value
+        draw.rectangle((0,0,self._size[0],self._size[1]), outline=0, fill=0)
+        draw.rectangle((0,0,len,self._size[1]), outline=1, fill=1)
+
