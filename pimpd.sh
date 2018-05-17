8077c72e3bcdc560b57b655950767b53cf1c7309
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2018-05-17 02:50:30 -0700

    run script fix

--- pimpd.sh
+++ pimpd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 cd /home/pi/pimpd/pimpd
 /bin/date >> /var/log/pimpd.log
-/usr/bin/python pimpd.py >> /var/log/pimpd.log 2>&1
+exec /usr/bin/python pimpd.py >> /var/log/pimpd.log 2>&1
